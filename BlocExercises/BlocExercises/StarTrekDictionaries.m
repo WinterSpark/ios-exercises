20128625803b0c4bcf8abded9f2302268a87278d
Mo Botan <foadbotan@gmail.com>
2015-12-23 23:45:55 +1100

    complete - 14 Dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,26 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
+    id favoriteDrink = characterDictionary[@"favorite drink"];
+    if (favoriteDrink != nil && [favoriteDrink isKindOfClass:[NSString class]]) {
+        return favoriteDrink;
+    }
     return @"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    return [charactersArray valueForKey:@"favorite drink"];
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *mutableCharacterDictionaryWithQuote = [[NSMutableDictionary alloc] initWithDictionary:characterDictionary];
+    [mutableCharacterDictionaryWithQuote setObject:@"Make it so" forKey:@"quote"];
+    
+    NSDictionary *characterDictionaryWithQuote = [[NSDictionary alloc] initWithDictionary:mutableCharacterDictionaryWithQuote];
+    
+    return characterDictionaryWithQuote;
 }
 
 @end
