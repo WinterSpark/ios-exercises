6265cbd6761e88a9320d5b226ea6afd486942155
Mo Botan <foadbotan@gmail.com>
2015-12-24 12:38:48 +1100

    complete - 15 Simple Data Types and Objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,39 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    return [NSNumber numberWithInt:([number intValue] * 2)];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSInteger smallerInteger = 0;
+    NSInteger largerInteger = 0;
+    
+    if (number < otherNumber) {
+        smallerInteger = number;
+        largerInteger = otherNumber;
+    } else {
+        smallerInteger = otherNumber;
+        largerInteger = number;
+    }
+    
+    NSMutableArray *mutableArrayOfNumbersBetweenNumberAndOtherNumber = [NSMutableArray new];
+    
+    while (smallerInteger <= largerInteger) {
+        [mutableArrayOfNumbersBetweenNumberAndOtherNumber addObject:[NSNumber numberWithInteger:smallerInteger]];
+        smallerInteger++;
+    }
+    
+    return [NSArray arrayWithArray:mutableArrayOfNumbersBetweenNumberAndOtherNumber];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSArray *sortedArrayOfNumbers = [arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+    
+    return [sortedArrayOfNumbers[0] integerValue];
 }
 
 @end
