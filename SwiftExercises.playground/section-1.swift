011e1f1586aa41655e8fd1b57aa98e0516e356fb
Mo Botan <foadbotan@gmail.com>
2015-12-25 16:02:55 +1100

    complete - 18 Swift vs. Objective-C

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is " + cheese
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -23,10 +23,15 @@ Arrays & Dictionaries
 let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+var newNumberArray = numberArray + [5]
+
 
 let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+var newNumberDictionary = numberDictionary
+newNumberDictionary[5] = "five"
+
 
 /*
 
@@ -36,9 +41,15 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for var i in 1...10 {
+    print(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for var i in 1..<11 {
+    print(i)
+}
 
 let worf = [
     "name": "Worf",
@@ -58,7 +69,13 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> [String] {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var favoriteDrinks : [String] = []
+    for var character in characters {
+        if let drink = character["favorite drink"] {
+            favoriteDrinks.append(drink)
+        }
+    }
+    return favoriteDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -75,7 +92,10 @@ func emailFromUserDict(userDict : [String : String]) -> String {
     // Return the user's email address from userDict, or return "" if they don't have one
     
     // WORK HERE
-    return "user@example.com"
+    if let emailAddress = userDict["email"] {
+        return emailAddress
+    }
+    return ""
 }
 
 
@@ -101,6 +121,22 @@ let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
 
+func joinWithSemicolon(groceryList: [String]) -> String {
+    var result = ""
+    for item in groceryList {
+        if item == groceryList[0] {
+            result += item
+        } else {
+            result += ";" + item
+        }
+    }
+    return result
+}
+
+joinWithSemicolon(strings)
+
+strings.joinWithSeparator(";")
+
 let expectedOutput = "milk;eggs;bread;challah"
 
 /*
@@ -113,3 +149,16 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+var sortedCerealOne = cerealArray.sort({ (cerealOne : String, cerealTwo : String) -> Bool in
+    return cerealOne < cerealTwo
+})
+sortedCerealOne
+
+
+var sortedCerealTwo = cerealArray.sort({cerealOne, cerealTwo in return cerealOne < cerealTwo})
+sortedCerealOne
+
+
+var sortedCerealThree = cerealArray.sort(<)
+sortedCerealThree
